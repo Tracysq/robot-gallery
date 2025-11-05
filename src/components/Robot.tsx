ecfdf11807d9d2fa112f3a24e553e6e7fa03ef80
sunqi <kiki20100908@163.com>
2025-11-05 16:55:06 +0800

    Robot 组件样式

--- src/components/Robot.tsx
+++ src/components/Robot.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./Robot.module.css";
 interface RobotProps {
-  id: number,
-  name: string,
-  email: string
+  id: number;
+  name: string;
+  email: string;
 }
 
-const Robot : React.FC<RobotProps> = ({ id, name, email }) => {
-  return <li>
-    <img src={`https://robohash.org/${id}`} alt="robot" />
-    <h2>{ name }</h2>
-    <p>{ email }</p>
-  </li>
-}
+const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
+  return (
+    <div className={styles.cardContainer}>
+      <img src={`https://robohash.org/${id}`} alt="robot" />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
-export default Robot
+export default Robot;
