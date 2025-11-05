c6b996fd47c17c417ade040cee3c473b70ede892
sunqi <kiki20100908@163.com>
2025-11-05 17:46:58 +0800

    添加 assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./assets/images/logo.svg";
 import robots from "./mockdata/robots.json";
 import Robot from "./components/Robot";
 import styles from "./App.module.css";
@@ -7,9 +7,13 @@ import styles from "./App.module.css";
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} alt="logo" className={styles.appLogo}/>
+        <h1>机器人在线购物平台</h1>
+      </div>
       <div className={styles.robotList}>
         {robots.map((r) => (
-          <Robot id={r.id} name={r.name} email={r.email} />
+          <Robot key={r.id} id={r.id} name={r.name} email={r.email} />
         ))}
       </div>
     </div>
